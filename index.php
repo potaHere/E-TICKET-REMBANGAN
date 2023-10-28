6b9a8faf871ee5564825ad6c0261ce7cd4b401c8
faizbangett <148948420+faizbangett@users.noreply.github.com>
2023-10-28 16:20:15 +0700

    mariwes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rembangan</title>
     <link rel="stylesheet" href="css/style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
@@ -22,7 +22,7 @@
         <nav class="navbar">
             <a href="#home">Home</a>
             <a href="#features">Tentang</a>
-            <a href="#download">Lokasi</a>
+            <a href="#lokasi">Lokasi</a>
             <a href="#footer">Contact</a>
             <a href="#" class="btn">Log in</a>
         </nav>
@@ -38,15 +38,19 @@
     <section class="home" id="home">
         <div class="content">
             <h1>Rembangan</h1>
-            <p>Rembangan adalah wisata yang berada di dataran tinggi Jember. Puncak Rembangan berada di ketinggian 650 mdpl, di kaki gunung Argopuro. 
-                Wisata ini dikenal sebagai wisata pegunungan yang menawarkan banyak keindahan alam dan beberapa fasilitas yang mendukung. Suhu di kawasan Puncak Rembangan ini bisa mencapai 14 derajat celcius.</p>
+            <p style="text-indent: 30px" >Rembangan adalah wisata yang berada di dataran tinggi Jember. Puncak Rembangan berada di ketinggian 650 mdpl, di kaki gunung Argopuro. 
+                Wisata ini dikenal sebagai wisata pegunungan yang menawarkan banyak keindahan alam dan beberapa fasilitas yang mendukung. 
+                Suhu di kawasan Puncak Rembangan ini bisa mencapai 14 derajat celcius.</p>
+            <p style="text-indent: 30px">Puncak Rembangan berlokasi di Desa Kemuning Lor, Kecamatan Arjasa, Jember, Jawa Timur. 
+                Dari pusat kota Jember jaraknya sekitar 12 km dengan menempuh perjalanan sekitar 42 menit untuk mencapai puncak Rembangan.</p>
+
 
             <!-- <a href="#" class="home-btn">get it on chrome</a> -->
         </div>
 
 
         <div class="image">
-            <img src="images/illustration-hero.svg" alt="illustration-hero">
+            <img src="images/rembangan 1.jpg" alt="illustration-hero">
         </div>
     </section>
 
@@ -54,23 +58,32 @@
 
     <!-- features sectin starts  -->
 
-    <section class="features" id="features">
+    <section class="tentang" id="features">
         <div class="heading">
-            <h1>Tentang</h1>
-            <p>Our aim is to make it quick and easy for you to access your favorite websites. your bookmarks sync between your devices so you can access them on the go</p>
+            <h1>Tentang Wisata Rembangan</h1>
+            <p id="penjelasan">Kawasan wisata ini dikelola oleh pemerintah daerah Jember. Untuk memasuki kawasan Puncak Rembangan memerlukan harga tiket sebesar Rp7.500 per orang di hari biasa. 
+                Sedangkan di hari libur, pengunjung harus membayar tiket sebesar Rp10.000. Beberapa fasilitas yang disediakan oleh Wisata Rembangan yaitu:</p>
+                <p> - Hotel</p>
+                <p> - Cafe</p>
+                <p> - Kolam renang</p>
+                <p> - Camping Ground </p>
         </div>
 
 
         <div class="row">
             <!-- 1 Tab  -->
-            <div class="image">
-                <img src="images/illustration-features-tab-1.svg" alt="illustration-features-tab-1">
+            <div class="image" id="Hotel">
+                <img src="images/hotel 1.jpg" alt="illustration-features-tab-1">
             </div>
 
 
-            <div class="content">
+            <div class="content" id="Hotel">
                 <h1>Hotel</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sint odit tempora, maiores quos qui quisquam accusamus ad officiis sapiente.</p>
+                <p style="text-indent: 30px">Hotel yang berada di Desa Kemuning Lor, Kecamatan Arjasa, Kabupaten Jember tersebut memang dikenal sangat kental dengan sejarah kedatangan bangsa Belanda ke Indonesia.
+                Bangunan-bangunan kamar dan resto bergaya klasik, yang berada pada ketinggian lebih dari 600 Mdpl, dengan sejumlah kamar penginapan yang didesain minimalis dengan dilengkapi dua 
+                kursi dan satu meja pada setiap teras, dibuat mengarah langsung pada hamparan luas kota Jember, akan membuat pengunjung dapat segera merasakan kesan yang jauh berbeda dengan menginap di daerah perkotaan.
+                harga dari setiap kamar dipatok dengan nominal yang cukup bervariasi, yakni mulai harga Rp. 175.000 hingga Rp. 1.575.000. Dengan fasilitas berupa double/twin bed, televisi, kamar mandi, bath tub, AC dan air panas.
+                </p>
                 <!-- <a href="#" class="all-btn">more info</a> -->
             </div>
 
@@ -79,40 +92,55 @@
             <!-- 2 Tab  -->
             <div class="content">
                 <h1>Kolam Renang</h1>
-                <p>Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.</p>
+                <p>Kolam renang Rembangan merupakan destinasi wisata pemandian yang berlokasi di dataran tinggi. Kolam ini didukung dengan pemandangan yang indah dan banyak wahana bermain. Untuk bisa menikmati segarnya pemandian di kolam 
+                    renang rembangan, hanya perlu membayar karcis sebesar Rp10 ribu. Kolam renang ini buka tiap hari Senin hingga Minggu pada pukul 07.00 WIB sampai pukul 18.00 WIB. Cukup datang ke Jl. Rembangan, Kemuning Lor, Arjasa, Jember untuk dapat merasakan segarnya mandi di dataran tinggi.</p>
                 <!-- <a href="#" class="all-btn">more info</a> -->
             </div>
 
             <div class="image">
-                <img src="images/illustration-features-tab-2.svg" alt="illustration-features-tab-2">
+                <img src="images/kolam 1.jpg" alt="illustration-features-tab-2">
             </div>
 
             <!-- 2 Tab  -->
 
             <div class="image">
-                <img src="images/illustration-features-tab-3.svg" alt="illustration-features-tab-3">
+                <img src="images/resto 1.jpg" alt="illustration-features-tab-3">
             </div>
 
             <div class="content">
-                <h1>Restoran</h1>
-                <p>Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.</p>
+                <h1>Cafe</h1>
+                <p>Café yang satu ini menyediakan pemandangan indah di puncak ketinggian pegunungan yang sejuk dan menyegarkan. Café yang berada di puncak rembangan ini bernuansa kayu dengan aksen jawa yang kental dan dilengkapi dengan bangku kayu diluar untuk menikmati pemandangan pegunungan. 
+                    Café ini berada disekitar Rembangan, Darungan, Kemuning Lor, Kecamatan Arjasa, Kabupaten Jember. Tempat ini juga fasilitas kolam renang dan berudara dingin, sehingga cocok sebagai tempat nongkrong bersama keluarga</p>
                 <!-- <a href="#" class="all-btn">more info</a> -->
             </div>
 
             <div class="content">
                 <h1>Camping Ground</h1>
-                <p>Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.</p>
+                <p>Spot yang asik untuk camping bersama teman, yakni Camping Ground Puncak Rembangan Jember. Ketinggian tempat ini menawarkan pemandangan epik dengan tumbuhan hijau disekitarnya dan bisa melihat Kota Jember dari ketinggian. Menjadi tempat favorit untuk berkemah, saat malam hari akan 
+                    tampak lampu kota dari kejauhan yang indah. Menjelang pagi kita akan disuguhi pemandangan sunrise yang indah dari ufuk timur. Harga tiket masuk pun terjangkau, mulai dari Rp. 20 ribu per orang, kita sudah dapat mendirikan tenda di kawasan ini.
+</p>
                 <!-- <a href="#" class="all-btn">more info</a> -->
             </div>
 
             <div class="image">
-                <img src="images/illustration-features-tab-2.svg" alt="illustration-features-tab-2">
+                <img src="images/Camping ground 1.jpg" alt="illustration-features-tab-2">
             </div>
 
 
             <!-- 3 Tab  -->
 
         </div>
+    </section>
+
+    <section class="location" id="lokasi">
+    <div class="container text-center">
+  <div class="row d-flex justify-content-md-center">
+    <h1>Lokasi Wisata Rembangan</h1>
+    <div class="col-sm-auto" id="peta">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15800.898839275545!2d113.6904683!3d-8.0785502!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6eb6b22b4390b%3A0xcdcfa9792af9847c!2sHotel%20Rembangan!5e0!3m2!1sid!2sid!4v1698464765683!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+
     </section>
 
 
@@ -167,19 +195,9 @@
                <h3>quick links</h3>
                <a href="#"><i class="fas fa-chevron-right"></i>home</a>
                <a href="#"><i class="fas fa-chevron-right"></i>features</a>
-               <a href="#"><i class="fas fa-chevron-right"></i>download</a>
+               <a href="#"><i class="fas fa-chevron-right"></i>location</a>
                <a href="#"><i class="fas fa-chevron-right"></i>contact</a>
             </div>
-            
-            
-            <div class="box">
-                <h3>our services</h3>
-                <a href="#"><i class="fas fa-chevron-right"></i>bookmark extension</a>
-                <a href="#"><i class="fas fa-chevron-right"></i>chrome themes</a>
-                <a href="#"><i class="fas fa-chevron-right"></i>firefox themes</a>
-                <a href="#"><i class="fas fa-chevron-right"></i>screen recorder extension</a>
-                <a href="#"><i class="fas fa-chevron-right"></i>screen shot extension</a>
-           </div>
 
            <div class="box">
                <h3>contact info</h3>
@@ -202,7 +220,7 @@
 
 
        <div class="credit">created by
-           <a href="https://www.youtube.com/channel/UCK5YMqyy_fjAtwgu9hjxXJg" target="_blank"><span>Pranav Raj</span> | all rights reserved</a>
+           <a href="https://www.youtube.com/channel/UCK5YMqyy_fjAtwgu9hjxXJg" target="_blank"><span>Kelompok Gacor</span> | all rights reserved</a>
        </div>
    </section>
 
@@ -210,6 +228,8 @@
 
 
     <script src="js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
