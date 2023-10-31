a036a3c6cdf2d88919146462ed2c8f1e1a1cf2dd
potaHere <potanut12@gmail.com>
2023-11-01 00:43:37 +0700

    penambahan
    rapiin about

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,19 @@
         });
     });
 
+
+    // navbar hide
+        let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".navbar").style.top = "0";
+    } else {
+        document.querySelector(".navbar").style.top = "-100%";
+    }
+    prevScrollpos = currentScrollPos;
+    }
+
     // go to up button
     const goToTopButton = document.getElementById('go-to-top-button');
 
